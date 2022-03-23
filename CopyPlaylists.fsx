7ebd4d7f7374b1da1f0132a91ca258679f3adb2e
Matthew Herman <mattherman11@gmail.com>
2022-03-23 16:38:16 -0500

    Removed class library and went back to script only

--- CopyPlaylists.fsx
+++ CopyPlaylists.fsx
@@ -1,5 +1,77 @@
-#r "Spotify/bin/Debug/net6.0/Spotify.dll"
+#r "nuget: FSharp.Json"
 
-open Spotify.Api
+open System
+open System.Net
+open System.Net.Http
+open System.Net.Http.Headers
+open FSharp.Json
 
-let user = getCurrentUser "api-token"
+module Models =
+    type Profile = {
+        [<JsonField("id")>]
+        Id: string;
+        [<JsonField("email")>]
+        Email: string;
+        [<JsonField("display_name")>]
+        DisplayName: string;
+    }
+
+    type Playlist = {
+        [<JsonField("id")>]
+        Id: string;
+        [<JsonField("name")>]
+        Name: string;
+    }
+
+    type Playlists = {
+        [<JsonField("limit")>]
+        Limit: int;
+        [<JsonField("offset")>]
+        Offset: int;
+        [<JsonField("total")>]
+        Total: int;
+        [<JsonField("items")>]
+        Items: list<Playlist>
+    }
+
+module Api =
+
+    let client = new HttpClient()
+
+    client.BaseAddress <- new Uri("https://api.spotify.com")
+
+    let createRequestMessage method uri =
+        let request = new HttpRequestMessage()
+        request.RequestUri <- new Uri(uri)
+        request.Method <- method
+        request
+
+    let withAuthorization token (request: HttpRequestMessage) =
+        request.Headers.Authorization <- new AuthenticationHeaderValue("Bearer", token)
+        request
+
+    let sendRequest<'Response> request = async {
+        let! response = Async.AwaitTask <| client.SendAsync(request)
+        let! responseContent = Async.AwaitTask <| response.Content.ReadAsStringAsync()
+        let result = 
+            match response.StatusCode with
+            | HttpStatusCode.OK ->
+                Ok (Json.deserialize<'Response> responseContent)
+            | _ ->
+                Error responseContent
+        return result
+    }
+
+    let getCurrentUser token = async {
+        let request =
+            createRequestMessage HttpMethod.Get "https://api.spotify.com/v1/me"
+            |> withAuthorization token
+        return! sendRequest<Models.Profile> request
+    }
+
+    let getCurrentUserPlaylists token = async {
+        let request =
+            createRequestMessage HttpMethod.Get "https://api.spotify.com/v1/me/playlists"
+            |> withAuthorization token
+        return! sendRequest<Models.Playlists> request
+    }
