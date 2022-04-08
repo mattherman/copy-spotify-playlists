f41f1a53cb5c12465c6a59d380be5fb6bf9ec389
Matthew Herman <mattherman11@gmail.com>
2022-04-08 15:52:38 -0500

    Improved error handling

--- CopyPlaylists.fsx
+++ CopyPlaylists.fsx
@@ -21,6 +21,13 @@ module AsyncResult =
         | Error e -> return Error e
     }
 
+    let mapError (mapper: 'TError -> 'UError) (asyncResult: Async<Result<'T, 'TError>>): Async<Result<'T, 'UError>> = async {
+        let! result = asyncResult
+        match result with
+        | Ok value -> return Ok value
+        | Error e -> return Error (mapper e)
+    }
+
 module Models =
     type Profile = {
         Id: string;
@@ -79,6 +86,17 @@ module Models =
         SnapshotId: string;
     }
 
+    type ErrorDetail = {
+        Error: {| Status: int; Message: string |}
+    }
+
+    type Error =
+    | GetUserError of ErrorDetail
+    | GetPlaylistsError of ErrorDetail
+    | GetTracksError of ErrorDetail
+    | CreatePlaylistError of ErrorDetail
+    | AddPlaylistItemsError of ErrorDetail
+
 module Api =
     open System.Text
     open System.Net.Mime
@@ -105,32 +123,32 @@ module Api =
         request.Headers.Authorization <- new AuthenticationHeaderValue("Bearer", token)
         request
 
-    let send<'Response> request (expectedStatusCodes: HttpStatusCode list)= async {
+    let send<'Response, 'ErrorResponse> request (expectedStatusCodes: HttpStatusCode list)= async {
         let! response = Async.AwaitTask <| client.SendAsync(request)
         let! responseContent = Async.AwaitTask <| response.Content.ReadAsStringAsync()
         if expectedStatusCodes |> List.contains response.StatusCode then
             return Ok (Json.deserializeEx<'Response> config responseContent)
         else
-            return Error responseContent
+            return Error (Json.deserializeEx<'ErrorResponse> config responseContent)
     }
 
-    let get<'Response> token path = async {
+    let get<'Response, 'ErrorResponse> token path = async {
         let request =
             createRequestMessage HttpMethod.Get path
             |> withAuthorization token
-        return! send<'Response> request [ HttpStatusCode.OK ]
+        return! send<'Response, 'ErrorResponse> request [ HttpStatusCode.OK ]
     }
 
-    let post<'Request, 'Response> token (body: 'Request) path = async {
+    let post<'Request, 'Response, 'ErrorResponse> token (body: 'Request) path = async {
         let request =
             createRequestMessage HttpMethod.Post path
             |> withJsonBody body
             |> withAuthorization token
-        return! send<'Response> request [ HttpStatusCode.Created ]
+        return! send<'Response, 'ErrorResponse> request [ HttpStatusCode.Created ]
     }
 
-    let rec getAllPages<'Response> token pageUri : Async<Result<list<'Response>, string>> = async {
-        let pageResult = get<Models.PagedResponse<'Response>> token pageUri
+    let rec getAllPages<'Response, 'ErrorResponse> token pageUri : Async<Result<list<'Response>, 'ErrorResponse>> = async {
+        let pageResult = get<Models.PagedResponse<'Response>, 'ErrorResponse> token pageUri
         return! pageResult
             |> AsyncResult.bind(fun page ->
                 match page.Next with
@@ -147,31 +165,36 @@ module Api =
                     })
     }
 
-    let getCurrentUser token : Async<Result<Models.Profile, string>> = async {
-        return! get<Models.Profile> token "v1/me"
+    let getCurrentUser token = async {
+        return! get<Models.Profile, Models.ErrorDetail> token "v1/me"
+        |> AsyncResult.mapError Models.GetUserError
     }
 
     let getAllCurrentUserPlaylists token = async {
-        return! getAllPages<Models.Playlist> token "v1/me/playlists"
+        return! getAllPages<Models.Playlist, Models.ErrorDetail> token "v1/me/playlists"
+        |> AsyncResult.mapError Models.GetPlaylistsError
     }
 
     let getAllTracksForPlaylist token (playlist: Models.Playlist) = async {
-        return! getAllPages<Models.TrackDetails> token playlist.Tracks.Href
+        return! getAllPages<Models.TrackDetails, Models.ErrorDetail> token playlist.Tracks.Href
+        |> AsyncResult.mapError Models.GetTracksError
     }
 
     let createPlaylist token userId (createPlaylistRequest: Models.CreatePlaylistRequest) = async {
-        return! post<Models.CreatePlaylistRequest, Models.Playlist> token createPlaylistRequest $"v1/users/{userId}/playlists"
+        return! post<Models.CreatePlaylistRequest, Models.Playlist, Models.ErrorDetail> token createPlaylistRequest $"v1/users/{userId}/playlists"
+        |> AsyncResult.mapError Models.CreatePlaylistError
     }
 
     let addItemsToPlaylist token playlistId (addPlaylistItemsRequest: Models.AddPlaylistItemsRequest) = async {
-        return! post<Models.AddPlaylistItemsRequest, Models.AddPlaylistItemsResponse> token addPlaylistItemsRequest $"v1/playlists/{playlistId}/tracks"
+        return! post<Models.AddPlaylistItemsRequest, Models.AddPlaylistItemsResponse, Models.ErrorDetail> token addPlaylistItemsRequest $"v1/playlists/{playlistId}/tracks"
+        |> AsyncResult.mapError Models.AddPlaylistItemsError
     }
 
 let createPlaylistWithTracks token (createPlaylistRequest: Models.CreatePlaylistRequest) (tracks: Models.TrackDetails list) = async {
     let trackUris =
         tracks
         |> List.map (fun track -> track.Track.Uri)
-        |> List.truncate 100 // TODO: Limit of 100 per request, need to update to make multiple requests
+        //|> List.truncate 100 // TODO: Limit of 100 per request, need to update to make multiple requests
 
     return! Api.getCurrentUser token
     |> AsyncResult.bind (fun userProfile -> Api.createPlaylist token userProfile.Id createPlaylistRequest)
@@ -189,19 +212,20 @@ let copyPlaylist srcToken destToken (playlist: Models.Playlist) = async {
     |> AsyncResult.bind (createPlaylistWithTracks destToken playlistRequest)
 }
 
-let copyPlaylistsTest token =
+let copyPlaylists srcToken destToken =
     let playlists = 
-        Api.getAllCurrentUserPlaylists token
-        |> AsyncResult.map (List.skip 15 >> List.take 5)
+        Api.getAllCurrentUserPlaylists srcToken
+        |> AsyncResult.map (List.skip 3 >> List.take 1)
         |> Async.RunSynchronously
 
     match playlists with
     | Ok playlists ->
+        printfn $"Copying {playlists.Length} playlist(s)"
         for playlist in playlists do
             printfn $"Copying playlist: {playlist.Name}"
-            let copyResult = copyPlaylist token token playlist |> Async.RunSynchronously
+            let copyResult = copyPlaylist srcToken destToken playlist |> Async.RunSynchronously
             match copyResult with
             | Ok _ -> printfn "=> Success"
-            | Error msg -> printfn $"=> Failed: {msg}"
-    | Error msg ->
-        printfn $"Unable to retrieve playlists: {msg}"
+            | Error errorDetail -> printfn $"=> Failed: {errorDetail}"
+    | Error errorDetail ->
+        printfn $"Failed: {errorDetail}"
